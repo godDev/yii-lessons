37c8d057cec4af1835ba0a87591266dcb4f20bc8
zucky <usakrg@gmail.com>
2022-11-14 10:53:52 +0600

    init commit

---
+++ frontend/controllers/LessonsController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace frontend\controllers;
+
+use frontend\models\ResendVerificationEmailForm;
+use frontend\models\VerifyEmailForm;
+use Yii;
+use yii\base\InvalidArgumentException;
+use yii\web\BadRequestHttpException;
+use yii\web\Controller;
+use yii\filters\VerbFilter;
+use yii\filters\AccessControl;
+use common\models\LoginForm;
+use frontend\models\PasswordResetRequestForm;
+use frontend\models\ResetPasswordForm;
+use frontend\models\SignupForm;
+use frontend\models\ContactForm;
+
+/**
+ * Site controller
+ */
+class LessonsController extends Controller
+{
+    public function actionIndex()
+    {
+        return $this->render('index');
+    }
+
+}
